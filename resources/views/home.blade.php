8c927f25fa76e6392f46c93f4e90540831869b5d
akmaldwi123 <akmaldwi4@gmail.com>
2023-07-19 20:02:31 +0800

    akmal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,9 @@
      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
      <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css')!!}">
-     <link rel="stylesheet" href="{!! asset("css/font-awesome.min.css")!!}>
-     <link rel="stylesheet" href="{!! asset('css/animate.css')!!}>
-     <link rel="stylesheet" href="{!! asset ('css/owl.carousel.css')!!}>
+     <link rel="stylesheet" href="{!! asset("css/font-awesome.min.css")!!}">
+     <link rel="stylesheet" href="{!! asset('css/animate.css')!!}">
+     <link rel="stylesheet" href="{!! asset ('css/owl.carousel.css')!!}">
      <link rel="stylesheet" href={!! asset("css/owl.theme.default.min.css")!!}>
 
      <!-- MAIN CSS -->
