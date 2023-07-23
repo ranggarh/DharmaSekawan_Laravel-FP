33a7d6ff3a2650da47e88f1e1973a6fcd0a38af3
ranggarh <ranggarhoke@gmail.com>
2023-07-23 17:43:30 +0700

    merged testing

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -4,21 +4,7 @@
 
      <title>apa aja dlu</title>
      <meta charset="UTF-8">
-     {{-- <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-     <meta name="description" content="">
-     <meta name="keywords" content="">
-     <meta name="author" content="style">
-     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-     <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css')!!}">
-     <link rel="stylesheet" href="{!! asset("css/font-awesome.min.css")!!}">
-     <link rel="stylesheet" href="{!! asset('css/animate.css')!!}">
-     <link rel="stylesheet" href="{!! asset ('css/owl.carousel.css')!!}">
-     <link rel="stylesheet" href={!! asset("css/owl.theme.default.min.css")!!}>
-
-     <!-- MAIN CSS -->
-     <link rel="stylesheet" type="text/css" href="{!! asset('css/style.css') !!}">
-     @vite('resources/sass/app.scss') --}}
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
